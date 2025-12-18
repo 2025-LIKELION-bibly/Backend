f51eff8abc54f4f71e331758137b3ba747fa7c02
leewatertrue <leewatertrue@naver.com>
2025-12-19 00:00:01 +0900

    fix: cors

--- src/main/java/likelion/bibly/global/config/WebConfig.java
+++ src/main/java/likelion/bibly/global/config/WebConfig.java
@@ -18,7 +18,7 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/api/**")
+		registry.addMapping("/**")
 			.allowedOrigins("http://localhost:3000", "http://localhost:5173")
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
 			.allowedHeaders("*")
